3f9be9b52539e3280ad00a95c0f01b71d9dc3fb6
Riyaj Tamboli <154032387+riyajtamboli@users.noreply.github.com>
2025-10-07 01:19:00 +0530

    Update 1_max.c

--- 5th_Day/1_max.c
+++ 5th_Day/1_max.c
@@ -4,7 +4,5 @@ int num1, num2, num3;
 printf("Enter the Number : ");
 scanf("%d%d%d", &num1, &num2, &num3);
 
-
-// (num1>num2 && num1> num3)?printf("maximum : %d",num1):(num2>num3)?printf("maximum : %d",num2):printf("maximum : %d",num3);
 (num1>num2)?(num1>num3)?printf("maximum : %d",num1):printf("maximum : %d",num3):(num2>num3)?printf("maximum : %d",num2):printf("maximum : %d",num3);
 }
